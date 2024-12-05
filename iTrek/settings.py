b0dae34eba02d91d0a961f51b7bcca0083d83216
Ignacio Villela <ivillela@talana.com>
2024-12-04 23:08:35 -0300

    add csrf settings

--- iTrek/settings.py
+++ iTrek/settings.py
@@ -30,6 +30,10 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://itrek.cl',
+    'https://www.itrek.cl'  # Si usas un subdominio.
+]
 
 # Application definition
 
